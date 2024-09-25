5c731f50947ef6af6ad17dd99db94c70d6e2ba4d
wennjiee <1229368677@qq.com>
2024-09-25 09:55:55 +0800

    update train scripts

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,13 @@
 import os
 import glob
 if __name__ == '__main__':
-    train_name = 'zxy'
+    train_name = 'lc_128'
+    cmd0 = f'python data_utils/process.py data/{train_name}/{train_name}.mp4'
     cmd1 = f'python main.py data/{train_name}/ --workspace trial/{train_name}_head/ -O --iters 100000'
     cmd2 = f'python main.py data/{train_name}/ --workspace trial/{train_name}_head/ -O --iters 125000 --finetune_lips --patch_size 32'
+    preprocessed_data = f'data/{train_name}/transforms_train.json'
+    if not os.path.exists(preprocessed_data):
+        os.system(cmd0)
     print("******************start training head******************")
     os.system(cmd1)
     print("******************finished training head******************")
